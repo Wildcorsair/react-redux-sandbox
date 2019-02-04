fd8cdaa1ece8b17c2c1d05fc882c57627726a61b
Roman Berezhnoi <berezhnoi.f5@gmail.com>
2019-02-04 16:10:22 +0200

    Added Redux update and refactoring the code

--- src/index.js
+++ src/index.js
@@ -1,63 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore, bindActionCreators } from 'redux';
 
-// import { inc, dec, rnd } from './actions';
+import Counter from './Counter';
 import * as actions from './actions';
 import reducer from './reducer';
 
 const store = createStore(reducer);
 const { dispatch } = store;
 
-
-// const incDispatch = () => dispatch(inc());
-// const decDispatch = () => dispatch(dec());
-// const rndDispatch = (payload) => dispatch(rnd(payload));
-
-// After refactoring
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args));
-// };
-//
-// const incDispatch = bindActionCreator(inc, dispatch);
-// const decDispatch = bindActionCreator(dec, dispatch);
-// const rndDispatch = bindActionCreator(rnd(payload, dispatch));
-
-// And with standard Redux function bindActionCreators
-// const incDispatch = bindActionCreators(inc, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-// Mass binding
-// const { incDispatch, decDispatch, rndDispatch } = bindActionCreators({
-//   incDispatch: inc,
-//   decDispatch: dec,
-//   rndDispatch: rnd
-// }, dispatch);
-
-// And one more refactoring
 const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
-// document.getElementById('inc').addEventListener('click', () => incDispatch());
-//
-// document.getElementById('dec').addEventListener('click', () => decDispatch());
-//
-// document.getElementById('rnd').addEventListener('click', () => {
-//   const randomValue = Math.floor(Math.random() * 10);
-//   rndDispatch(randomValue);
-// });
-
-document.getElementById('inc').addEventListener('click', inc);
-
-document.getElementById('dec').addEventListener('click', dec);
-
-document.getElementById('rnd').addEventListener('click', () => {
-  const randomValue = Math.floor(Math.random() * 10);
-  rnd(randomValue);
-});
-
 const update = () => {
-  document
-    .getElementById('counter')
-    .innerHTML = store.getState();
+  ReactDOM.render(
+    <Counter
+      counter={store.getState()}
+      inc={inc}
+      dec={dec}
+      rnd={() => {
+        const value = Math.floor(Math.random() * 10);
+        rnd(value);
+      }}
+    />,
+    document.getElementById('root')
+  );
 };
 
+update();
 store.subscribe(update);
